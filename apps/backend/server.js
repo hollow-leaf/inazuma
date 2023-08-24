162396fe53f080b235b9a289139de69343b9a1ec
kidneyweak <kidneyweak@gmail.com>
2023-08-24 13:39:13 +0800

    feat: deploy docker

---
+++ apps/backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Test Docker');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
