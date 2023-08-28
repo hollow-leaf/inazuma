31dd305df5957a4ff013df72e82dd14ae159b35f
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2023-08-28 14:52:34 +0800

    feat: upload backend

---
+++ apps/backend/src/hypercert/hypercert.js
@@ -0,0 +1,64 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const axios = require('axios');
+
+export async function fetch_cert_address(address) {
+  try {
+    const apiUrl = 'https://api.thegraph.com/subgraphs/name/hypercerts-admin/hypercerts-testnet';
+
+    // Define your GraphQL query
+    const graphqlQuery = `
+      {
+        claimTokens(where:{owner: ${"\""+address+"\""}}) {
+          claim {
+            uri
+          }
+          id
+          tokenID
+          units
+        }
+      }
+    `;
+
+    const response = await axios.post(apiUrl, {
+      query: graphqlQuery,
+    });
+
+    // The response data will contain the results of your query
+    const data = response.data.data;
+    return data
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+export async function fetch_cert_uri(address, uri) {
+  try {
+    const apiUrl = 'https://api.thegraph.com/subgraphs/name/hypercerts-admin/hypercerts-testnet';
+
+    // Define your GraphQL query
+    const graphqlQuery = `
+      {
+        claimTokens(where:{owner: ${"\""+address+"\""}},claim_: {uri: ${"\""+uri+"\""}}) {
+          claim {
+            uri
+          }
+          id
+          tokenID
+          units
+        }
+      }
+    `;
+
+    const response = await axios.post(apiUrl, {
+      query: graphqlQuery,
+    });
+
+    // The response data will contain the results of your query
+    const data = response.data.data;
+    return data
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
