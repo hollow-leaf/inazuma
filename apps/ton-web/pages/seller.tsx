29a0b136fad040c0b4fdc76e562f6d78b0a6f96b
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2023-09-21 15:15:21 +0800

    feat: profile page add

--- apps/ton-web/pages/seller.tsx
+++ apps/ton-web/pages/seller.tsx
@@ -1,22 +1,34 @@
 import SellerTable from "../components/sellerTable";
-import { useAccount } from "wagmi";
 import { getAssets } from "../service/api";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "../components/loading";
-import ProvideButton from "../components/provideButton";
 import Alert from "../components/alert";
+import {iswallet, connectwallet} from "../service/wallect"
+import { ton_address } from "../service/market";
 
 function Page() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["getsale"],
+    queryFn: async () =>{
+      if(await iswallet()){
+        return await ton_address()
+      }else{
+        return ""
+      }
+    },
+      retry: 10,
+      cacheTime: 100*5
+  });
 
-
+  if (isLoading) return <Loading />;
   return (
     <>
-      {false ? (
-        <Loading />
+      {data==""||data==undefined? (
+        <button onClick={connectwallet}>Connect Wallet</button>
       ) : (
         <div className="w-1/2 mx-auto my-10">
-          <SellerTable />
+          <SellerTable address={data} />
         </div>
       )}
     </>
